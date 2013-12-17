997fd9358575c096f3f3b88d8568128e1bae2936
Anant Subramanian <anant.subramanian15@gmail.com>
2013-12-17 17:03:48 +0530

    nCr

---
+++ ncr.cpp
@@ -0,0 +1,22 @@
+long long C(int n,int k)
+{
+  long long ans=1;
+  k = (k > (n-k)) ? (n-k) : k;
+  int j = 1;
+  for(; j <= k; j++, n--)
+  {
+    if(n%j == 0)
+    {
+      ans *= n/j;
+    }
+	  else if(ans%j == 0)
+    {
+      ans = (ans/j)*n;
+    }
+	  else
+    {
+      ans = (ans*n)/j;
+    }
+  }
+  return ans;
+}
